c33fe7773edc4d72a5b175efa52d8d6577ea8a01
0918nobita <nobita.0918@gmail.com>
2021-09-09 14:56:10 +0900

    check/eval コマンド、関数についての説明を追記した

--- hello.lean
+++ hello.lean
@@ -1,7 +1,8 @@
+-- #check コマンドは項の型を出力する
 #check 1
-
 #check fun (x : Nat) => x + 1
 
+-- #eval コマンドは即座に式を評価し結果を出力する
 #eval (fun (x : Nat) => x + 1) 3
 
 def fact : Nat → Nat
@@ -30,3 +31,10 @@ theorem ex1 : p ∨ q → q ∨ p := by
   cases h with
   | Or.inl h1 => apply Or.inr; exact h1
   | Or.inr h2 => apply Or.inl; exact h2
+
+-- Lean の主要なコンセプトは「型」と「関数」
+-- すべての Lean プログラムの最も基本的な要素は、名前空間に集約される関数である。
+-- 関数は入力に対して出力を生成するために働き、それらは名前空間に集約される。
+-- 名前空間はものをグループ化する主要な方法である。
+-- 関数は def コマンドを用いて定義される。
+-- def コマンドは関数に名前を与え、その引数を定義する
