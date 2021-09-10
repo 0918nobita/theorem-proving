5668c2596df93c52fef4c3ec368f9fabb0e79f95
0918nobita <nobita.0918@gmail.com>
2021-09-11 01:45:54 +0900

    与えられた ascii 文字がアルファベットかどうか判定する関数を追加した

--- coq/lexical_analysis.v
+++ coq/lexical_analysis.v
@@ -4,3 +4,14 @@ Require Import Coq.Strings.Ascii.
 Definition isWhite (c : ascii) : bool :=
   let n := nat_of_ascii c in
   (n =? 32) || (n =? 9) || (n =? 10) || (n =? 13).
+
+Definition isLowerAlpha (c : ascii) : bool :=
+  let n := nat_of_ascii c in
+  (97 <=? n) && (n <=? 122).
+
+Definition isUpperAlpha (c : ascii) : bool :=
+  let n := nat_of_ascii c in
+  (65 <=? n) && (n <=? 90).
+
+Definition isAlpha (c : ascii) : bool :=
+  isLowerAlpha c || isUpperAlpha c.
